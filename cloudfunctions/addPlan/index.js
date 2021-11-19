eb1f80cd279e4e530d84eb16e5bb8ce48c71908e
hizouxx <xuxiang.zou@yudesports.com>
2021-11-19 17:13:37 +0800

    Add：新增云函数

---
+++ cloudfunctions/addPlan/index.js
@@ -0,0 +1,27 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  // const wxContext = cloud.getWXContext()
+
+  try {
+    return await db.collection('plan').add({
+      data: {
+        openid: event.openid,
+        nickName: event.nickName,
+        avatarUrl: event.avatarUrl,
+        createTime: event.createTime, // 创建日期
+        value: event.value, // 内容
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
